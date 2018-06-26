247ef9863649efd5dd427ac02f4958c9ef60b580
Andrey Yudov <yudovv@gmail.com>
2018-06-26 20:54:23 +0300

    File Upload

--- bot_handlers.py
+++ bot_handlers.py
@@ -5,13 +5,14 @@ from db import users_db # Импортируем базу данных
 
 @bot.message_handler(commands=['start']) # Выполняется, когда пользователь нажимает на start
 def send_welcome(message):
-    # Если пользователя нет в базе
-    if not users_db.find_one({"chat_id": message.chat.id}):
-        users_db.insert_one({"chat_id" : message.chat.id})
         bot.send_message(message.chat.id, HELLO_MESSAGE)
-    # Если пользователь есть в базе
-    else:
-        bot.send_message(message.chat.id, HELLO_AGAIN_MESSAGE)
+#    # Если пользователя нет в базе
+#    if not users_db.find_one({"chat_id": message.chat.id}):
+#        users_db.insert_one({"chat_id" : message.chat.id})
+#        bot.send_message(message.chat.id, HELLO_MESSAGE)
+#    # Если пользователь есть в базе
+#    else:
+#        bot.send_message(message.chat.id, HELLO_AGAIN_MESSAGE)
 
 
 @bot.message_handler(content_types=["text"]) # Любой текст
@@ -19,7 +20,7 @@ def repeat_all_messages(message):
     if message.text == 'Тополиный пух':
         bot.send_message(message.chat.id, ANSWER)
     else:
-        bot.send_message(message.chat.id, "блаблабла(я пишу только одно сообщение)\nНаисано @ AndreyYudov(https://t.me/AndreyYudov)")
+        bot.send_message(message.chat.id, "блаблабла(я пишу только одно сообщение)\nНаисано @ AndreyYudov(https://t.me/AndreyYudov)\n(Хочешь прикол - напиши 'Тополиный пух'")
 
 
 if __name__ == '__main__':
