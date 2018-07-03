beca22db5d24d9ac177389b049f6db0fc2678dac
Andrey Yudov <yudovv@gmail.com>
2018-07-03 19:32:00 +0300

    Add buttons

--- bot_handlers.py
+++ bot_handlers.py
@@ -1,6 +1,7 @@
 from bot import bot # Импортируем объект бота
 from messages import * # Инмпортируем все с файла сообщений
 from db import users_db # Импортируем базу данных
+from telebot import types
 #import bot
 
 @bot.message_handler(commands=['help'])
@@ -26,13 +27,13 @@ def repeat_all_messages(message):
     else:
         bot.send_message(message.chat.id, ELSE_ANSWER)
 
-#@bot.message_handler(commands = ['button'])
-#def inline(message):
-    #key = types.InlineKeyboardMarkup()
-    #but_1 = types.InlineKeyboardButton(text = "Android", callback_data="Android pressed")
-    #but_2 = types.InlineKeyboardButton(text = "IOS", callback_data="IOS pressed")
-    #key.add(but_1,but_2)
-    #bot.send_message(message.chat.id, "What is your OS?", reply_markup=key)
+@bot.message_handler(commands = ['button'])
+def inline(message):
+    key = types.InlineKeyboardMarkup()
+    but_1 = types.InlineKeyboardButton(text = "Android", callback_data="Android pressed")
+    but_2 = types.InlineKeyboardButton(text = "IOS", callback_data="IOS pressed")
+    key.add(but_1,but_2)
+    bot.send_message(message.chat.id, "What is your OS?", reply_markup=key)
 
 
 if __name__ == '__main__':
