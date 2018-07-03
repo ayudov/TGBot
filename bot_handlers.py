82dc0cf66f6bed39254f3c7cde7816d188e4bc7d
Andrey Yudov <yudovv@gmail.com>
2018-07-03 19:49:52 +0300

    Add buttons

--- bot_handlers.py
+++ bot_handlers.py
@@ -8,9 +8,17 @@ from telebot import types
 def send_help(message):
         bot.send_message(message.chat.id, HELP_MESSAGE)
 
-#@bot.message_handler(commands=['start']) # Выполняется, когда пользователь нажимает на start
-#def send_welcome(message):
- #       bot.send_message(message.chat.id, HELLO_MESSAGE)
+@bot.message_handler(commands=['start']) # Выполняется, когда пользователь нажимает на start
+def send_welcome(message):
+        bot.send_message(message.chat.id, HELLO_MESSAGE)
+
+        #key = types.InlineKeyboardMarkup()
+        #but_1 = types.InlineKeyboardButton(text="Android", callback_data="Android pressed")
+        #but_2 = types.InlineKeyboardButton(text="IOS", callback_data="IOS pressed")
+        #key.add(but_1, but_2)
+        #bot.send_message(message.chat.id, "What is your OS?", reply_markup=key)
+
+
 #    # Если пользователя нет в базе
 #    if not users_db.find_one({"chat_id": message.chat.id}):
 #        users_db.insert_one({"chat_id" : message.chat.id})
@@ -19,14 +27,12 @@ def send_help(message):
 #    else:
 #        bot.send_message(message.chat.id, HELLO_AGAIN_MESSAGE)
 
-@bot.message_handler(commands = ['start'])
-def inline(message):
-    key = types.InlineKeyboardMarkup()
-    but_1 = types.InlineKeyboardButton(text = "Android", callback_data="Android pressed")
-    but_2 = types.InlineKeyboardButton(text = "IOS", callback_data="IOS pressed")
-    key.add(but_1,but_2)
-    bot.send_message(message.chat.id, "What is your OS?", reply_markup=key)
-
+@bot.message_handler(commands = ['url'])
+def url(message):
+    markup = types.InlineKeyboardMarkup()
+    btn_my_site= types.InlineKeyboardButton(text='Наш сайт', url='https://habrahabr.ru')
+    markup.add(btn_my_site)
+    bot.send_message(message.chat.id, "Нажми на кнопку и перейди на наш сайт.", reply_markup = markup)
 
 @bot.message_handler(content_types=["text"]) # Любой текст
 def repeat_all_messages(message):
