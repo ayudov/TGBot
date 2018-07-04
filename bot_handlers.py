a7042c070697d807060e1826cc9e1ba3eff78091
Andrey Yudov <yudovv@gmail.com>
2018-07-04 17:32:03 +0300

    add id

--- bot_handlers.py
+++ bot_handlers.py
@@ -38,29 +38,17 @@ def send_welcome(message):
 
     bot.send_message(message.chat.id, HELLO_MESSAGE, reply_markup=user_markup)
 
-@bot.message_handler(content_types=["text"]) # Любой текст
-def repeat_all_messages(message):
-    if message.text == 'Тополиный пух' or message.text == 'тополиный пух':
+ @bot.message_handler(content_types=["text"]) # Любой текст
+ def repeat_all_messages(message):
+     if message.text == 'Тополиный пух' or message.text == 'тополиный пух':
         bot.send_message(message.chat.id, ANSWER)
-    elif message.text == "Получить ссылку":
+     elif message.text == "Получить ссылку на Google spreadsheet":
         bot.send_message(message.chat.id, URL_MESSAGE)
-    elif message.text == "Помощь":
+     elif message.text == "Помощь":
         bot.send_message(message.chat.id,HELP_MESSAGE)
-    elif message.text == "Добавить себя":
+     elif message.text == "Добавить себя в таблицу":
         sheet.append_row([str(message.chat.id), str(message.from_user.id), str(message.from_user.first_name), str(message.from_user.last_name), str(message.from_user.username)])
-        bot.send_message(message.chat.id, 'Ваша информация была добавлена')
-    else:
-        bot.send_message(message.chat.id, ELSE_ANSWER)
-
-    elif message.text == "Найти всё по ID пользователя":
-        user_markup_find = telebot.types.ReplyKeyboardMarkup(True, True)
-        user_markup_find.row('Главное меню')
-        bot.send_message(message.chat.id, 'Пожалуйста, введите ID (9 символов)', reply_markup=user_markup_find)
-
-        #bot.send_message(message.chat.id, 'Перешел в пункт поиска пользователей')
-
-    elif message.text == "Главное меню":
-        send_welcome(message)
+        bot.send_message(message.chat.id, 'Ваш id был добавлен')
 
 
 if __name__ == '__main__':
